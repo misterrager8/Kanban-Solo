0988b00ee2dd95f57602b50d683337131d63f4e4
Chemllei Joseph <chemllei0304@gmail.com>
2022-06-29 18:07:57 -0400

    Implem pywebview for desktop envs

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from kanban_solo import create_app
 import config
+import webview
 
 app = create_app(config)
 
 if __name__ == "__main__":
-    app.run()
+    webview.create_window(
+        "Kanban Solo",
+        app,
+        frameless=True,
+        fullscreen=True,
+        easy_drag=False,
+        text_select=True,
+    )
+    webview.start(debug=True)
